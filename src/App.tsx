f1cac6e7eb2a9e1adbebb1a719abb1d538a43419
Matthew Ko <iammatthewko114@gmail.com>
2023-10-17 15:36:45 +0800

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline">
+          Tailwind is working
+        </h1>
       </header>
     </div>
   );
